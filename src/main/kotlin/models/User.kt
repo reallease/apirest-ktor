f7372bdb6904e82a59bd00b1109aa6a62650c510
reallease <iamthomasdavid2005@gmail.com>
2025-02-06 20:00:42 -0300

    modify model user

--- src/main/kotlin/models/User.kt
+++ src/main/kotlin/models/User.kt
@@ -1,10 +1,10 @@
 package com.thomasd.models
 
 import kotlinx.serialization.Serializable
+import java.util.UUID
 
-@Serializable
-class User(
-    val id: String,
+data class User( // classe somente para o modelo
+    val id: UUID,
     val username: String,
     val email: String,
     val password: String
